4e4cd151e76ce948f2ee22b5d0149bf69905ffcc
Byron Edmonds Decosse <60893158+ByronED@users.noreply.github.com>
2020-03-02 13:10:17 -0500

    pizzacost

---
+++ PizzaCost/PizzaCost/frmPizzaCost.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace PizzaCost
+{
+    partial class frmPizzaCost
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.txtDiameter = new System.Windows.Forms.TextBox();
+            this.lblTotalQuestion = new System.Windows.Forms.Label();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mniFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(434, 144);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(89, 33);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.AutoSize = true;
+            this.lblQuestion.Location = new System.Drawing.Point(187, 86);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(200, 13);
+            this.lblQuestion.TabIndex = 1;
+            this.lblQuestion.Text = "enter the diameter of the pizza (in inches)";
+            // 
+            // txtDiameter
+            // 
+            this.txtDiameter.Location = new System.Drawing.Point(423, 86);
+            this.txtDiameter.Name = "txtDiameter";
+            this.txtDiameter.Size = new System.Drawing.Size(100, 20);
+            this.txtDiameter.TabIndex = 2;
+            // 
+            // lblTotalQuestion
+            // 
+            this.lblTotalQuestion.AutoSize = true;
+            this.lblTotalQuestion.Location = new System.Drawing.Point(187, 164);
+            this.lblTotalQuestion.Name = "lblTotalQuestion";
+            this.lblTotalQuestion.Size = new System.Drawing.Size(133, 13);
+            this.lblTotalQuestion.TabIndex = 3;
+            this.lblTotalQuestion.Text = "the total cost including tax:";
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Location = new System.Drawing.Point(377, 164);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(25, 13);
+            this.lblAnswer.TabIndex = 4;
+            this.lblAnswer.Text = "???";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniFile});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mniFile
+            // 
+            this.mniFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniExit});
+            this.mniFile.Name = "mniFile";
+            this.mniFile.Size = new System.Drawing.Size(35, 20);
+            this.mniFile.Text = "file";
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(180, 22);
+            this.mniExit.Text = "exit";
+            this.mniExit.Click += new System.EventHandler(this.MniExit_Click);
+            // 
+            // frmPizzaCost
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.lblTotalQuestion);
+            this.Controls.Add(this.txtDiameter);
+            this.Controls.Add(this.lblQuestion);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmPizzaCost";
+            this.Text = "Pizza Cost By Byron .ED";
+            this.Load += new System.EventHandler(this.FrmPizzaCost_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.TextBox txtDiameter;
+        private System.Windows.Forms.Label lblTotalQuestion;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mniFile;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
+    }
+}
+
---
+++ PizzaCost/PizzaCost/frmPizzaCost.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PizzaCost
+{
+    public partial class frmPizzaCost : Form
+    {
+        public frmPizzaCost()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            //declare local variables 
+            double diameter, cost;
+
+            //convert the string from the text box to a double 
+            diameter = double.Parse(txtDiameter.Text);
+
+            //calculate the cost of the pizza
+            cost = 0.5 * diameter + 0.99 + 0.75 * 1.13;
+
+            //insert the cost of the pizza in to their respective label
+            this.lblAnswer.Text = Convert.ToString(cost) + "$";
+
+            //display the cost of the pizza into its respective label
+            this.lblAnswer.Show();
+
+        }
+
+        private void FrmPizzaCost_Load(object sender, EventArgs e)
+        {
+            //hide the label
+            lblAnswer.Hide();
+        }
+
+        private void MniExit_Click(object sender, EventArgs e)
+        {
+            //close the program
+            this.Close();
+        }
+    }
+}
